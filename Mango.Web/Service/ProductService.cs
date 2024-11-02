e3a3d25f7a5b19df284dc462e90da9d232084af0
staratul <atulji.star@gmail.com>
2024-11-02 17:43:24 +0530

    Add Shopping Cart API

---
+++ Mango.Web/Service/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class ProductService : IProductService
+    {
+        private IBaseService _baseService;
+        public ProductService(IBaseService baseService) 
+        { 
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> CreateProductAsync(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Utility.SD.ApiType.POST,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "/api/product",
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteProductAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Utility.SD.ApiType.DELETE,
+                Url = SD.ProductAPIBase + "/api/product/" + id,
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllProductsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto { 
+                ApiType = Utility.SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product",
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductByIdAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Utility.SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product/" + id,
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateProductAsync(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Utility.SD.ApiType.PUT,
+                Data = productDto,
+                Url = SD.ProductAPIBase + "/api/product",
+            });
+        }
+    }
+}
