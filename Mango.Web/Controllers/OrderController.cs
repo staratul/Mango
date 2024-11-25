0a01f860dbba981a6cdfe4ff56e1d9375ec527bc
staratul <atulji.star@gmail.com>
2024-11-25 23:29:56 +0530

    order details and status

--- Mango.Web/Controllers/OrderController.cs
+++ Mango.Web/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace Mango.Web.Controllers
@@ -21,8 +22,67 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        public async Task<IActionResult> OrderDetails(int orderId)
+        {
+            OrderHeaderDto orderHeaderDto = new OrderHeaderDto();
+            string userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+
+            var response = await _orderService.GetOrder(orderId);
+
+            if (response != null && response.IsSuccess)
+            {
+                orderHeaderDto = JsonConvert.DeserializeObject<OrderHeaderDto>(Convert.ToString(response.Result));
+            }
+
+            if(!User.IsInRole(SD.RoleAdmin) && userId != orderHeaderDto.UserId)
+            {
+                return NotFound(); 
+            }
+
+            return View(orderHeaderDto);
+        }
+
+        [HttpPost("OrderReadyForPickup")]
+        public async Task<IActionResult> OrderReadyForPickup(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_ReadyForPickup);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetails), new {orderId = orderId});
+            }
+            return View();
+        }
+
+        [HttpPost("CompleteOrder")]
+        public async Task<IActionResult> CompleteOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Completed);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetails), new { orderId = orderId });
+            }
+            return View();
+        }
+
+        [HttpPost("CancelOrder")]
+        public async Task<IActionResult> CancelOrder(int orderId)
+        {
+            var response = await _orderService.UpdateOrderStatus(orderId, SD.Status_Cancelled);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Status updated successfully";
+                return RedirectToAction(nameof(OrderDetails), new { orderId = orderId });
+            }
+            return View();
+        }
+
         [HttpGet]
-        public IActionResult GetAll() 
+        public IActionResult GetAll(string status) 
         {
             IEnumerable<OrderHeaderDto> list;
             string userId = string.Empty;
@@ -36,6 +96,20 @@ namespace Mango.Web.Controllers
             if(response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(response.Result));
+                switch(status)
+                {
+                    case "approved":
+                        list = list.Where(u => u.Status == SD.Status_Approved);
+                        break;
+                    case "readyforpickup":
+                        list = list.Where(u => u.Status == SD.Status_ReadyForPickup);
+                        break;
+                    case "cancelled":
+                        list = list.Where(u => u.Status == SD.Status_Cancelled || u.Status == SD.Status_Refunded);
+                        break;
+                    default:
+                        break;
+                }
             } else
             {
                 list = new List<OrderHeaderDto>();
--- Mango.Web/wwwroot/js/order.js
+++ Mango.Web/wwwroot/js/order.js
@@ -1,18 +1,42 @@
 ﻿var dataTable;
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("approved")) {
+        loadDataTable("approved");
+    } else {
+        if (url.includes("readyforpickup")) {
+            loadDataTable("readyforpickup");
+        } else {
+            if (url.includes("cancelled")) {
+                loadDataTable("cancelled");
+            } else {
+                loadDataTable("all");
+            }
+        }
+    }
 })
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $("#tblData").DataTable({
-        "ajax": { url: "/order/getall" },
+        "ajax": { url: "/order/getall?status=" + status },
         "columns": [
             {data: 'orderHeaderId', "width": "5%"},
             {data: 'name', "width": "20%"},
             {data: 'email', "width": "25%"},
             {data: 'phone', "width": "10%"},
             {data: 'status', "width": "10%"},
-            {data: 'orderTotal', "width": "10%"}
+            {data: 'orderTotal', "width": "10%"},
+            {
+                data: 'orderHeaderId',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/order/orderDetails?orderId=${data}" class="btn btn-primary mx-2">
+                            <i class="bi bi-pencil-square"></i>
+                        </a>
+                    </div>`
+                },
+                "width": "10%"
+            }
         ]
     });
 }
